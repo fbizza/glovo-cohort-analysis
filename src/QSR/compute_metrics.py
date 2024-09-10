dfb3681f6cb3f9bf31da82a4fccca4b3f2e2b19d
Francesco Bizzarri <francesco.bizzarri@glovoapp.com>
2024-09-10 17:02:52 +0200

    Add LTO metric and refactor code

--- src/QSR/compute_metrics.py
+++ src/QSR/compute_metrics.py
@@ -1,140 +1,140 @@
 from extract_qsr_customers import get_qsr_data
 from utils import run_queries
 from plot_results import plot_dictionaries
+from pprint import pprint
+
+def get_customer_ids_str(df):
+    """Convert customer IDs to a comma-separated string."""
+    customer_ids = df['customer_id'].astype(int).tolist()
+    return ', '.join(str(customer_id) for customer_id in customer_ids)
+def build_queries(customer_ids_str):
+    """Build SQL queries for various metrics."""
+    queries = {
+        "stores": f"""
+            SELECT customer_id, COUNT(DISTINCT o.store_name) AS n_stores
+            FROM delta.central_order_descriptors_odp.order_descriptors_v2 o
+            WHERE order_final_status = 'DeliveredStatus'
+              AND order_parent_relationship_type IS NULL
+              AND customer_id IN ({customer_ids_str})
+            GROUP BY customer_id
+        """,
+        "frequency": f"""
+            SELECT customer_id, COUNT(o.order_id) AS order_frequency
+            FROM delta.central_order_descriptors_odp.order_descriptors_v2 o
+            WHERE order_final_status = 'DeliveredStatus'
+              AND order_parent_relationship_type IS NULL
+              AND customer_id IN ({customer_ids_str})
+            GROUP BY customer_id
+        """,
+        "retention": f"""
+            SELECT customer_id, COUNT(o.order_id) AS order_count
+            FROM delta.central_order_descriptors_odp.order_descriptors_v2 o
+            WHERE order_final_status = 'DeliveredStatus'
+              AND order_parent_relationship_type IS NULL
+              AND customer_id IN ({customer_ids_str})
+            GROUP BY customer_id
+            HAVING COUNT(o.order_id) >= 2
+        """,
+        "aov_cm": f"""
+            SELECT customer_id, ROUND(AVG(order_total_purchase_local*1.00), 2) AS aov,
+                   AVG(contribution_margin_eur) AS avg_contribution_margin_eur
+            FROM delta.central_order_descriptors_odp.order_descriptors_v2 o
+            LEFT JOIN delta.finance_financial_reports_odp.pnl_order_level f
+            ON o.order_id = f.order_id
+            WHERE order_final_status = 'DeliveredStatus'
+              AND order_parent_relationship_type IS NULL
+              AND customer_id IN ({customer_ids_str})
+            GROUP BY customer_id
+        """,
+        "negative_cm_percentage": f"""
+            SELECT customer_id,
+                   1.000 * COUNT(CASE WHEN contribution_margin_eur < 0 THEN o.order_id ELSE NULL END) / COUNT(o.order_id) AS negative_cm_orders_percentage
+            FROM delta.central_order_descriptors_odp.order_descriptors_v2 o
+            LEFT JOIN delta.finance_financial_reports_odp.pnl_order_level f
+            ON o.order_id = f.order_id
+            WHERE order_final_status = 'DeliveredStatus'
+              AND order_parent_relationship_type IS NULL
+              AND customer_id IN ({customer_ids_str})
+            GROUP BY customer_id
+        """,
+        "prime_orders": f"""
+            SELECT customer_id,
+                   COUNT(CASE WHEN o.order_is_prime THEN o.order_id ELSE NULL END) as prime_orders_count
+            FROM delta.central_order_descriptors_odp.order_descriptors_v2 o
+            LEFT JOIN delta.finance_financial_reports_odp.pnl_order_level f
+            ON o.order_id = f.order_id
+            WHERE order_final_status = 'DeliveredStatus'
+              AND order_parent_relationship_type IS NULL
+              AND customer_id IN ({customer_ids_str})
+            GROUP BY customer_id
+        """,
+        "promo_types": f"""
+            SELECT
+                1.0000*count(DISTINCT CASE WHEN discount_subtype = 'PROMOTION_TWO_FOR_ONE' THEN o.order_id ELSE NULL END) / count(DISTINCT o.order_id) AS two_for_one_promo_orders,1.0000*count(DISTINCT CASE WHEN discount_subtype = 'MKTG_PROMOCODE' THEN o.order_id ELSE NULL END) / count(DISTINCT o.order_id) AS mktg_promo_code_promo_orders,
+                1.0000*count(DISTINCT CASE WHEN discount_subtype = 'PRIME' THEN o.order_id ELSE NULL END) / count(DISTINCT o.order_id) AS prime_promo_orders,
+                1.0000*count(DISTINCT CASE WHEN discount_subtype = 'PROMOTION_PERCENTAGE_DISCOUNT' THEN o.order_id ELSE NULL END) / count(DISTINCT o.order_id) AS percentage_discount_promo_orders,
+                1.0000*count(DISTINCT CASE WHEN discount_subtype = 'PROMOTION_FREE_DELIVERY' THEN o.order_id ELSE NULL END) / count(DISTINCT o.order_id) free_delivery_promo_orders,
+                1.0000*count(DISTINCT CASE WHEN discount_subtype = 'PROMOTION_BASKET_DISCOUNT' THEN o.order_id ELSE NULL END) / count(DISTINCT o.order_id) AS basket_discount_promo_orders,
+                1.0000*count(DISTINCT CASE WHEN discount_subtype = 'SEGMENTATION' THEN o.order_id ELSE NULL END) / count(DISTINCT o.order_id) AS segmentation_promo_orders,
+                1.0000*count(DISTINCT CASE WHEN discount_subtype = 'PROMOTION_FLAT_DELIVERY' THEN o.order_id ELSE NULL END) / count(DISTINCT o.order_id) AS flat_delivery_promo_orders
+            FROM delta.central_order_descriptors_odp.order_descriptors_v2 o
+            LEFT JOIN delta.growth_pricing_discounts_odp.pricing_discounts p
+            ON p.order_id = o.order_id
+            WHERE order_final_status = 'DeliveredStatus'
+              AND order_parent_relationship_type IS NULL
+              AND customer_id IN ({customer_ids_str})
+        """,
+        "lto": f"""
+            WITH latest_dates AS (
+                SELECT customer_id, MAX(p_date) AS latest_p_date
+                FROM delta.growth__customer_lto__odp.customer_lto
+                GROUP BY customer_id
+            ),
+            users_5y_total_orders AS (
+                SELECT lto.customer_id, lto.first_order_country,
+                       lto.predicted_orders_60mo + oh.number_of_orders AS user_5y_lto,
+                       ld.latest_p_date
+                FROM delta.growth__customer_lto__odp.customer_lto lto
+                FULL JOIN delta.growth__customer_lto__odp.customer_order_history oh
+                ON lto.customer_id = oh.customer_id AND lto.p_date = oh.p_date
+                JOIN latest_dates ld
+                ON lto.customer_id = ld.customer_id AND lto.p_date = ld.latest_p_date
+            )
+            SELECT avg(user_5y_lto) as avg_lto
+            FROM users_5y_total_orders
+            WHERE customer_id in ({customer_ids_str})
+        """
+    }
+    return queries
+def compute_metrics_for_cohort(df):
+    """Compute metrics for a given cohort."""
+    customer_ids_str = get_customer_ids_str(df)
+    queries = build_queries(customer_ids_str)
+    results = run_queries(list(queries.values()))
+    result_stores, result_frequency, result_retention, result_aov_cm, result_negative_cm_percentage, result_prime_orders, result_promo_types, result_lto = results
+    metrics = {
+        'avg_n_stores': round(float(result_stores['n_stores'].mean()), 3),
+        'avg_order_frequency': round(float(result_frequency['order_frequency'].mean()), 3),
+        'second_order_retention': round(len(result_retention) / len(df), 4),
+        'avg_aov': round(float(result_aov_cm['aov'].mean()), 2),
+        'avg_cm': round(float(result_aov_cm['avg_contribution_margin_eur'].mean()), 2),
+        'negative_cm_orders': round(float(result_negative_cm_percentage['negative_cm_orders_percentage'].mean()), 4),
+        'prime_users_conversion': round(len(result_prime_orders[result_prime_orders['prime_orders_count'] > 0]) / len(df), 4),
+        '5y_lto': float(result_lto['avg_lto'].values[0])
+    }
+    promo_orders_percentage = {col: float(round(result_promo_types[col].iloc[0], 4)) for col in result_promo_types.columns}
+    metrics.update(promo_orders_percentage)
+    return metrics
 
 def compute_metrics(customers_dfs):
+    """Compute metrics for all customer cohorts."""
     metrics_dict = {}
-
-    for i, df in enumerate(customers_dfs, start=1):
-
-        customer_ids = df['customer_id'].astype(int).tolist()
-        customer_ids_str = ', '.join(str(customer_id) for customer_id in customer_ids)
-        print(f"\nProcessing cohort {i} ({len(customer_ids)} customers)...")
-
-        # avg number of different stores customers ordered from
-        query_stores = f"""
-        SELECT
-            customer_id,
-            COUNT(DISTINCT o.store_name) AS n_stores
-        FROM delta.central_order_descriptors_odp.order_descriptors_v2 o
-        WHERE order_final_status = 'DeliveredStatus'
-          AND order_parent_relationship_type IS NULL
-          AND customer_id IN ({customer_ids_str})
-        GROUP BY customer_id
-        """
-
-        # avg frequency
-        query_frequency = f"""
-        SELECT
-            customer_id,
-            COUNT(o.order_id) AS order_frequency
-        FROM delta.central_order_descriptors_odp.order_descriptors_v2 o
-        WHERE order_final_status = 'DeliveredStatus'
-          AND order_parent_relationship_type IS NULL
-          AND customer_id IN ({customer_ids_str})
-        GROUP BY customer_id
-        """
-
-        # retention: percentage of customers who placed at least two orders
-        query_retention = f"""
-        SELECT
-            customer_id,
-            COUNT(o.order_id) AS order_count
-        FROM delta.central_order_descriptors_odp.order_descriptors_v2 o
-        WHERE order_final_status = 'DeliveredStatus'
-          AND order_parent_relationship_type IS NULL
-          AND customer_id IN ({customer_ids_str})
-        GROUP BY customer_id
-        HAVING COUNT(o.order_id) >= 2
-        """
-
-        # avg order value (AOV) and avg contribution margin (CM)
-        query_aov_cm = f"""
-              SELECT
-                  customer_id,
-                  ROUND(AVG(order_total_purchase_local*1.00), 2) AS aov,
-                  AVG(contribution_margin_eur) AS avg_contribution_margin_eur
-              FROM delta.central_order_descriptors_odp.order_descriptors_v2 o
-              LEFT JOIN delta.finance_financial_reports_odp.pnl_order_level f
-              ON o.order_id = f.order_id
-              WHERE order_final_status = 'DeliveredStatus'
-                AND order_parent_relationship_type IS NULL
-                AND customer_id IN ({customer_ids_str})
-              GROUP BY customer_id
-              """
-
-        # percentage of negative CM orders
-        query_negative_cm_percentage = f"""
-                SELECT
-                    customer_id,
-                    1.000 * COUNT(CASE WHEN contribution_margin_eur < 0 THEN o.order_id ELSE NULL END) / COUNT(o.order_id) AS negative_cm_orders_percentage
-                FROM delta.central_order_descriptors_odp.order_descriptors_v2 o
-                LEFT JOIN delta.finance_financial_reports_odp.pnl_order_level f
-                ON o.order_id = f.order_id
-                WHERE order_final_status = 'DeliveredStatus'
-                  AND order_parent_relationship_type IS NULL
-                  AND customer_id IN ({customer_ids_str})
-                GROUP BY customer_id
-                """
-
-        # for computing percentage of customers who became prime users
-        query_count_prime_orders = f"""
-                        SELECT
-                            customer_id,
-                            COUNT(CASE WHEN o.order_is_prime THEN o.order_id ELSE NULL END) as prime_orders_count
-                        FROM delta.central_order_descriptors_odp.order_descriptors_v2 o
-                        LEFT JOIN delta.finance_financial_reports_odp.pnl_order_level f
-                        ON o.order_id = f.order_id
-                        WHERE order_final_status = 'DeliveredStatus'
-                          AND order_parent_relationship_type IS NULL
-                          AND customer_id IN ({customer_ids_str})
-                        GROUP BY customer_id
-                        """
-
-        query_promo_types = f"""
-                                SELECT 
-                                    1.0000*count(DISTINCT CASE WHEN discount_subtype = 'PROMOTION_TWO_FOR_ONE' THEN o.order_id ELSE NULL END) / count(DISTINCT o.order_id) AS two_for_one_promo_orders,
-                                    1.0000*count(DISTINCT CASE WHEN discount_subtype = 'MKTG_PROMOCODE' THEN o.order_id ELSE NULL END) / count(DISTINCT o.order_id) AS mktg_promo_code_promo_orders,
-                                    1.0000*count(DISTINCT CASE WHEN discount_subtype = 'PRIME' THEN o.order_id ELSE NULL END) / count(DISTINCT o.order_id) AS prime_promo_orders,
-                                    1.0000*count(DISTINCT CASE WHEN discount_subtype = 'PROMOTION_PERCENTAGE_DISCOUNT' THEN o.order_id ELSE NULL END) / count(DISTINCT o.order_id) AS percentage_discount_promo_orders,
-                                    1.0000*count(DISTINCT CASE WHEN discount_subtype = 'PROMOTION_FREE_DELIVERY' THEN o.order_id ELSE NULL END) / count(DISTINCT o.order_id) free_delivery_promo_orders,
-                                    1.0000*count(DISTINCT CASE WHEN discount_subtype = 'PROMOTION_BASKET_DISCOUNT' THEN o.order_id ELSE NULL END) / count(DISTINCT o.order_id) AS basket_discount_promo_orders,
-                                    1.0000*count(DISTINCT CASE WHEN discount_subtype = 'SEGMENTATION' THEN o.order_id ELSE NULL END) / count(DISTINCT o.order_id) AS segmentation_promo_orders,
-                                    1.0000*count(DISTINCT CASE WHEN discount_subtype = 'PROMOTION_FLAT_DELIVERY' THEN o.order_id ELSE NULL END) / count(DISTINCT o.order_id) AS flat_delivery_promo_orders
-                                FROM delta.central_order_descriptors_odp.order_descriptors_v2 o LEFT JOIN delta.growth_pricing_discounts_odp.pricing_discounts p ON p.order_id = o.order_id
-                                WHERE order_final_status = 'DeliveredStatus'
-                                    AND order_parent_relationship_type IS NULL
-                                    AND customer_id IN ({customer_ids_str})
-                                """
-
-        # Run the queries
-        results = run_queries([query_stores, query_frequency, query_retention, query_aov_cm, query_negative_cm_percentage, query_count_prime_orders, query_promo_types])
-        result_stores, result_frequency, result_retention, result_aov_cm, result_negative_cm_percentage, result_prime_orders, result_promo_types = results
-        # Compute the metrics
-        avg_n_stores = round(float(result_stores['n_stores'].mean()), 3)
-        avg_order_frequency = round(float(result_frequency['order_frequency'].mean()), 3)
-        retention_rate = round(len(result_retention) / len(customer_ids), 4)
-        avg_aov = round(float(result_aov_cm['aov'].mean()), 2)
-        avg_cm_eur = round(float(result_aov_cm['avg_contribution_margin_eur'].mean()), 2)
-        negative_cm_orders_percentage = round(float(result_negative_cm_percentage['negative_cm_orders_percentage'].mean()), 4)
-        prime_users_percentage = round(len(result_prime_orders[result_prime_orders['prime_orders_count'] > 0]) / len(customer_ids), 4)
-        promo_orders_percentage = {col: float(round(result_promo_types[col].iloc[0], 4)) for col in result_promo_types.columns}
-
-
-        # Save metrics in the dictionary
-        metrics_dict[df.name] = {
-            'avg_n_stores': avg_n_stores,
-            'avg_order_frequency': avg_order_frequency,
-            'second_order_retention': retention_rate,
-            'avg_aov': avg_aov,
-            'avg_cm': avg_cm_eur,
-            'negative_cm_orders': negative_cm_orders_percentage,
-            'prime_users_conversion': prime_users_percentage,
-        }
-        metrics_dict[df.name].update(promo_orders_percentage)
+    for df in customers_dfs:
+        print(f"\nProcessing cohort {df.name} ({len(df)} customers)...")
+        metrics_dict[df.name] = compute_metrics_for_cohort(df)
     return metrics_dict
 
-# Example usage
+
 if __name__ == "__main__":
     qsr_customers, not_qsr_customers, mc_do = get_qsr_data('2024-01-01', '2024-01-31', ["McDonald''s"], update=False)
     qsr_customers.name = 'qsr_customers'
@@ -143,7 +143,13 @@ if __name__ == "__main__":
     customers_dfs = [mc_do, qsr_customers, not_qsr_customers]
     metrics_dict = compute_metrics(customers_dfs)
 
-    print("Metrics for McDo customers:", metrics_dict['McDo'])
-    print("Metrics for QSR customers:", metrics_dict['qsr_customers'])
-    print("Metrics for non-QSR customers:", metrics_dict['not_qsr_customers'])
+    print("Metrics for McDo customers:")
+    pprint(metrics_dict['McDo'])
+
+    print("\nMetrics for QSR customers:")
+    pprint(metrics_dict['qsr_customers'])
+
+    print("\nMetrics for non-QSR customers:")
+    pprint(metrics_dict['not_qsr_customers'])
+
     plot_dictionaries(metrics_dict)
--- src/QSR/plot_results.py
+++ src/QSR/plot_results.py
@@ -1,9 +1,7 @@
 import matplotlib.pyplot as plt
 import math
 
-
-def plot_dictionaries(dict_of_dicts, max_plots_per_row=3, colors=None):
-
+def plot_dictionaries(dict_of_dicts, max_plots_per_row=3, max_plots_per_page=6, colors=None):
     main_keys = list(dict_of_dicts.keys())
     sub_keys = list(dict_of_dicts[main_keys[0]].keys())
     num_main_keys = len(main_keys)
@@ -11,54 +9,109 @@ def plot_dictionaries(dict_of_dicts, max_plots_per_row=3, colors=None):
 
     # default colors if not provided
     if colors is None:
-        colors = plt.cm.get_cmap('tab10', num_main_keys).colors
+        colors = plt.colormaps['tab10'].colors
 
-    # Calculate the number of rows and columns
-    num_rows = math.ceil(num_sub_keys / max_plots_per_row)
-    num_cols = min(num_sub_keys, max_plots_per_row)
+    # Calculate the number of pages
+    num_pages = math.ceil(num_sub_keys / max_plots_per_page)
 
-    fig, axes = plt.subplots(num_rows, num_cols, figsize=(3 * num_cols, 3 * num_rows))
+    for page in range(num_pages):
+        start_idx = page * max_plots_per_page
+        end_idx = min(start_idx + max_plots_per_page, num_sub_keys)
+        current_sub_keys = sub_keys[start_idx:end_idx]
 
-    if num_rows > 1:
-        axes = axes.flatten()
-    else:
-        axes = [axes]
+        num_rows = math.ceil(len(current_sub_keys) / max_plots_per_row)
+        num_cols = min(len(current_sub_keys), max_plots_per_row)
 
-    for i, sub_key in enumerate(sub_keys):
-        values = [dict_of_dicts[main_key][sub_key] for main_key in main_keys]
-        axes[i].bar(range(num_main_keys), values, color=colors)
-        axes[i].set_title(sub_key)
-        axes[i].set_xticks(range(num_main_keys))
-        axes[i].set_xticklabels(main_keys)
+        fig, axes = plt.subplots(num_rows, num_cols, figsize=(3 * num_cols, 3 * num_rows))
 
-        # adjust y-axis to highlight differences
-        min_val = min(values)
-        max_val = max(values)
-        lower = min_val - 0.2 * max_val if min_val > 0 else min_val - 0.1 * abs(min_val)
-        upper = max_val + 0.2 * max_val
-        axes[i].set_ylim(lower, upper)
+        # Ensure axes is always a list
+        if num_rows > 1 or num_cols > 1:
+            axes = axes.flatten()
+        else:
+            axes = [axes]
 
-        # annotate bars with their values
-        for j, value in enumerate(values):
-            if sub_key in ['second_order_retention', 'negative_com_orders', 'prime_users_conversion', 'retention_percentage', 'negative_cm_orders'] or ('promo' in sub_key):
-                axes[i].text(j, value, f'{value*100:.2f}%', ha='center', va='bottom')
-            else:
-                axes[i].text(j, value, f'{value:.2f}', ha='center', va='bottom')
+        for i, sub_key in enumerate(current_sub_keys):
+            values = [dict_of_dicts[main_key][sub_key] for main_key in main_keys]
+            axes[i].bar(range(num_main_keys), values, color=colors)
+            axes[i].set_title(sub_key)
+            axes[i].set_xticks(range(num_main_keys))
+            axes[i].set_xticklabels(main_keys)
 
-    # hide any unused subplots, useful if n_subplots is not multiple of 3
-    for j in range(i + 1, len(axes)):
-        fig.delaxes(axes[j])
+            # adjust y-axis to highlight differences
+            min_val = min(values)
+            max_val = max(values)
+            lower = min_val - 0.2 * max_val if min_val > 0 else min_val - 0.1 * abs(min_val)
+            upper = max_val + 0.2 * max_val
+            axes[i].set_ylim(lower, upper)
 
-    plt.tight_layout()
-    plt.show()
+            # annotate bars with their values
+            for j, value in enumerate(values):
+                if sub_key in ['second_order_retention', 'negative_com_orders', 'prime_users_conversion', 'retention_percentage', 'negative_cm_orders'] or ('promo' in sub_key):
+                    axes[i].text(j, value, f'{value*100:.2f}%', ha='center', va='bottom')
+                else:
+                    axes[i].text(j, value, f'{value:.2f}', ha='center', va='bottom')
+
+        # hide any unused subplots, useful if n_subplots is not multiple of max_plots_per_row
+        for j in range(i + 1, len(axes)):
+            fig.delaxes(axes[j])
+
+        plt.tight_layout()
+        plt.show()
 
 if __name__ == "__main__":
     # example usage
     dict_of_dicts = {
-        'Dict1': {'avg_n_stores': 1.889, 'avg_order_frequency': 4.571, 'second_order_retention': 0.5734, 'avg_aov': 63.51, 'avg_cm': 1.13, 'negative_cm_orders': 0.2656, 'prime_users_conversion': 0.0202, 'two_for_one_promo_orders': 0.005, 'mktg_promo_code_promo_orders': 0.0858, 'prime_promo_orders': 0.0327, 'percentage_discount_promo_orders': 0.0411, 'free_delivery_promo_orders': 0.0231, 'basket_discount_promo_orders': 0.003, 'segmentation_promo_orders': 0.0569, 'flat_delivery_promo_orders': 0.0081},
-        'Dict2': {'avg_n_stores': 2.499, 'avg_order_frequency': 5.137, 'second_order_retention': 0.5773, 'avg_aov': 68.94, 'avg_cm': 1.34, 'negative_cm_orders': 0.2399, 'prime_users_conversion': 0.0378, 'two_for_one_promo_orders': 0.0086, 'mktg_promo_code_promo_orders': 0.0432, 'prime_promo_orders': 0.0712, 'percentage_discount_promo_orders': 0.1091, 'free_delivery_promo_orders': 0.0416, 'basket_discount_promo_orders': 0.0057, 'segmentation_promo_orders': 0.0472, 'flat_delivery_promo_orders': 0.0222}
+        'Dict1': {'5y_lto': 16.22171139995639,
+                 'avg_aov': 62.36,
+                 'avg_cm': 1.1,
+                 'avg_n_stores': 1.876,
+                 'avg_order_frequency': 4.47,
+                 'basket_discount_promo_orders': 0.0036,
+                 'flat_delivery_promo_orders': 0.008,
+                 'free_delivery_promo_orders': 0.0234,
+                 'mktg_promo_code_promo_orders': 0.1014,
+                 'negative_cm_orders': 0.2691,
+                 'percentage_discount_promo_orders': 0.0425,
+                 'prime_promo_orders': 0.0336,
+                 'prime_users_conversion': 0.0208,
+                 'second_order_retention': 0.5691,
+                 'segmentation_promo_orders': 0.0581,
+                 'two_for_one_promo_orders': 0.005},
+        'Dict2': {'5y_lto': 17.104947406230792,
+                 'avg_aov': 67.29,
+                 'avg_cm': 1.3,
+                 'avg_n_stores': 2.436,
+                 'avg_order_frequency': 4.844,
+                 'basket_discount_promo_orders': 0.0059,
+                 'flat_delivery_promo_orders': 0.0314,
+                 'free_delivery_promo_orders': 0.0414,
+                 'mktg_promo_code_promo_orders': 0.0426,
+                 'negative_cm_orders': 0.2083,
+                 'percentage_discount_promo_orders': 0.0797,
+                 'prime_promo_orders': 0.0545,
+                 'prime_users_conversion': 0.0258,
+                 'second_order_retention': 0.5716,
+                 'segmentation_promo_orders': 0.0508,
+                 'two_for_one_promo_orders': 0.0092},
+        'Dict3': {'5y_lto': 18.436153288870234,
+                 'avg_aov': 69.0,
+                 'avg_cm': 1.33,
+                 'avg_n_stores': 2.491,
+                 'avg_order_frequency': 5.238,
+                 'basket_discount_promo_orders': 0.0059,
+                 'flat_delivery_promo_orders': 0.0185,
+                 'free_delivery_promo_orders': 0.0412,
+                 'mktg_promo_code_promo_orders': 0.0419,
+                 'negative_cm_orders': 0.2469,
+                 'percentage_discount_promo_orders': 0.1151,
+                 'prime_promo_orders': 0.071,
+                 'prime_users_conversion': 0.039,
+                 'second_order_retention': 0.5806,
+                 'segmentation_promo_orders': 0.0466,
+                 'two_for_one_promo_orders': 0.0083}
+
     }
 
-    colors = ['blue', 'green']
+    # colors = ['blue', 'green']
 
-    plot_dictionaries(dict_of_dicts, colors=colors)
+    plot_dictionaries(dict_of_dicts)
